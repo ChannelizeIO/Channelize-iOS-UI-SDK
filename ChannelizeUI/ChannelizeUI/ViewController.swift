e69ec0688ff7e42dbbf4e1e79259efd754cc4a2d
rohitphogat19 <rohit.kumar@bigsteptech.com>
2020-04-17 18:19:10 +0530

    Intial Project Set up

---
+++ ChannelizeUI/ChannelizeUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChannelizeUI
+//
+//  Created by Ashish-BigStep on 4/17/20.
+//  Copyright © 2020 Channelize. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
